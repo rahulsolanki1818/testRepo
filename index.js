f0f32bb43cafab219226b3907afebe0aaaa4a0f9
Rahul Solanki <rs.singh1812@gmail.com>
2021-03-10 01:51:48 +0530

    index.js updated

--- index.js
+++ index.js
@@ -6,6 +6,34 @@ function test() {
   } else {
     console.log("no changes occured in git.");
   }
+}
 
-  //let add return statement
+function test() {
+  let changes = 0;
+
+  if (changes) {
+    console.log("changes occured in git");
+  } else {
+    console.log("no changes occured in git.");
+  }
+}
+
+function test() {
+  let changes = 0;
+
+  if (changes) {
+    console.log("changes occured in git");
+  } else {
+    console.log("no changes occured in git.");
+  }
+}
+
+function test() {
+  let changes = 0;
+
+  if (changes) {
+    console.log("changes occured in git");
+  } else {
+    console.log("no changes occured in git.");
+  }
 }
