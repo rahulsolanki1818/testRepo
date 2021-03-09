b276df9bb7576c3c2f1323ac6679ff67b84b5342
Rahul Solanki <rs.singh1812@gmail.com>
2021-03-10 01:52:09 +0530

    index.js modified

--- index.js
+++ index.js
@@ -17,23 +17,3 @@ function test() {
     console.log("no changes occured in git.");
   }
 }
-
-function test() {
-  let changes = 0;
-
-  if (changes) {
-    console.log("changes occured in git");
-  } else {
-    console.log("no changes occured in git.");
-  }
-}
-
-function test() {
-  let changes = 0;
-
-  if (changes) {
-    console.log("changes occured in git");
-  } else {
-    console.log("no changes occured in git.");
-  }
-}
